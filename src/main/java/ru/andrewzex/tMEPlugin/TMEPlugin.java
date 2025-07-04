d9ace3fa0ec4f33ffbc96c8e16ab9fb3e60e7d60
Andrew-Zex <ssurskih@gmail.com>
2025-07-04 23:35:52 +0500

    Initial commit

---
+++ src/main/java/ru/andrewzex/tMEPlugin/TMEPlugin.java
@@ -0,0 +1,17 @@
+package ru.andrewzex.tMEPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TMEPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
